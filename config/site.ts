8b8b914a939767eac478c1cdd48590a1797b48b2
Achref Gallaoui <gallaoui1999@gmail.com>
2026-02-02 21:56:30 +0100

    update: page title

--- config/site.ts
+++ config/site.ts
@@ -1,8 +1,8 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Next.js + HeroUI",
-  description: "Make beautiful websites regardless of your design experience.",
+  name: "Achref Gallaoui",
+  description: "Full-Stack Developer Portfolio",
   navItems: [
     {
       label: "Home",
